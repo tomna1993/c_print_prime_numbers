90e001fbe4936a11ead8c5faaf69ed1092c036ca
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-28 13:31:48 +0200

    Abstract prime calculation to a separate function

--- prime.c
+++ prime.c
@@ -1,27 +1,55 @@
 #include <stdio.h>
 #include <cs50.c>
 
+bool prime(int n);
+
 int main(void)
 {
-	int start = get_int("Start: ");
-	int end = get_int("End: ");
+	int start;
+	int end;
 
+	// Get the starting number from the user
+	do
+	{
+		start = get_int("Start: ");
+
+	} while (start < 1);
+	
+	// Get the ending number from the user
+	do
+	{
+		end = get_int("End: ");
+	
+	} while (end <= start);
+
+	// Iterate through the given numbers
 	for(start; start <= end; start++)
 	{
-		int divider = 2;
-		int modulus;
-
-		do
-		{
-			modulus = start % divider;
-
-			divider++;
-
-		} while (divider < start && modulus >= 1);
-
-		if(modulus != 0 || (modulus == 0 && start == 2))
+		if(prime(start))
 		{
 			printf("%i\n", start);
 		}
 	}
+}
+
+bool prime(int n)
+{
+	int divider = 2;
+	int modulus;
+
+	do
+	{
+		modulus = n % divider;
+
+		divider++;
+
+	// Don't check the number itself && run while until modulus is 0 && check just the numbers up to the half of the given number 
+	} while (divider < n && modulus >= 1 && divider < n/2);
+
+	if(modulus != 0 || (modulus == 0 && n == 2) && n != 1)
+	{
+		return true;
+	}
+	
+	return false;
 }
