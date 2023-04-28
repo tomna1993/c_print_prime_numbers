1fb91fb6219c9e92b4815cef29e8e899927e652e
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-28 13:43:13 +0200

    Change boolean expression

--- prime.c
+++ prime.c
@@ -5,10 +5,8 @@ bool prime(int n);
 
 int main(void)
 {
-	int start;
-	int end;
-
 	// Get the starting number from the user
+	int start;
 	do
 	{
 		start = get_int("Start: ");
@@ -16,6 +14,7 @@ int main(void)
 	} while (start < 1);
 	
 	// Get the ending number from the user
+	int end;
 	do
 	{
 		end = get_int("End: ");
@@ -46,7 +45,7 @@ bool prime(int n)
 	// Don't check the number itself && run while until modulus is 0 && check just the numbers up to the half of the given number 
 	} while (divider < n && modulus >= 1 && divider < n/2);
 
-	if(modulus != 0 || (modulus == 0 && n == 2) && n != 1)
+	if(modulus != 0 || (modulus == 0 && n == 2 && n != 1))
 	{
 		return true;
 	}
